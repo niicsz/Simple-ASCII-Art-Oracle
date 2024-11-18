ec35a73ce48fcbe0cebe60ceefb844851b2a8a33
Nicolas Bezerra Bini <78541669+niicsz@users.noreply.github.com>
2024-11-18 11:31:50 -0300

    Create SmileyFace.java

---
+++ SmileyFace.java
@@ -0,0 +1,12 @@
+public class SmileyFace {
+    public static void main(String[] args) {
+        System.out.println("   XXXXX   ");
+        System.out.println(" X       X ");
+        System.out.println("X  X   X  X");
+        System.out.println("X    X    X");
+        System.out.println("X  \\___/  X");
+        System.out.println(" X       X ");
+        System.out.println("   XXXXX   ");
+        System.out.println("            ");
+    }
+}
